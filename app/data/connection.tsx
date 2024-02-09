b8a7edee8c4cba237da59f0a63ae3eff63af49ca
boa50 <bruno.albuquerque50@gmail.com>
2024-02-09 14:19:49 -0300

    Changes to make the app work with Vercel

--- app/data/connection.tsx
+++ app/data/connection.tsx
@@ -1,19 +1,24 @@
+// TO ENCODE THE KEY FILE
+// const obj = {
+//     // PASTE THE OBJECT HERE
+// }
+
+// const encoded = btoa(JSON.stringify(obj))
+
+// console.log("Encoded: ", encoded)
+
+
 export const getGCPCredentials = () => {
-    // for Vercel, use environment variables
-    return process.env.GOOGLE_PRIVATE_KEY
-        ? {
-            credentials: {
-                client_email: process.env.GCLOUD_SERVICE_ACCOUNT_EMAIL,
-                private_key: process.env.GOOGLE_PRIVATE_KEY,
-            },
-            projectId: process.env.GCP_PROJECT_ID,
-        }
-        // for local development, use gcloud CLI
-        : { 
-            keyFilename: process.env.KEY_FILENAME,
-            projectId: process.env.PROJECT_ID,
-            scopes: [
-                'https://www.googleapis.com/auth/drive.readonly'
-            ] 
-        }
+    const credential = JSON.parse(atob(process.env.GCP_KEY_ENCODED || ""))
+
+    return {
+        credentials: {
+            client_email: credential.client_email,
+            private_key: credential.private_key,
+        },
+        projectId: process.env.PROJECT_ID,
+        scopes: [
+            'https://www.googleapis.com/auth/drive.readonly'
+        ] 
+    }
 }
