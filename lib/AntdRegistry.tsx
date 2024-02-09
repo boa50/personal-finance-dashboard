7e30af429e67e4eaa0fc026c7b8837b2fec1b123
boa50 <bruno.albuquerque50@gmail.com>
2024-02-09 14:30:14 -0300

    Another change to try deploying in Vercel

--- lib/AntdRegistry.tsx
+++ lib/AntdRegistry.tsx
@@ -1,9 +1,9 @@
 'use client'
 
 import React from 'react'
-import { createCache, extractStyle, StyleProvider } from '@ant-design/cssinjs'
+// import { createCache, extractStyle, StyleProvider } from '@ant-design/cssinjs'
 // if you are using Next.js 14, use below import instead. More info: https://github.com/ant-design/ant-design/issues/45567
-// import { createCache, extractStyle, StyleProvider } from '@ant-design/cssinjs/lib';
+import { createCache, extractStyle, StyleProvider } from '@ant-design/cssinjs/lib'
 import type Entity from '@ant-design/cssinjs/es/Cache'
 import { useServerInsertedHTML } from 'next/navigation'
 
